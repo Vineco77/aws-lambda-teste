a147183d102b628729c69f1881ffa7b64576b05b
Vinicius Ribeiro <vinicius.ribeiro@smartnx.com>
2025-04-20 18:26:54 -0300

    test(main): Testando como esta sendo meu Git Action

--- log.mjs
+++ log.mjs
@@ -1,4 +1,4 @@
 export function log(mensagem) {
-    console.log('Executando mensagem em arquivo externo log.mjs' + mensagem);
+    console.log('Executando mensagem em arquivo externo log.mjs. Após GitHub Action: ' + mensagem);
 
 }
