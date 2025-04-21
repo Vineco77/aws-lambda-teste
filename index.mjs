196241ef32067e9df9c3cf6f8170627d8a1962ef
Vinicius Ribeiro <vinicius.ribeiro@smartnx.com>
2025-04-21 10:14:28 -0300

    feat(main): Criando gerenciado do tamanho de arquivos que serão colocados no S3

--- index.mjs
+++ index.mjs
@@ -1,13 +1,24 @@
-import { log } from "./log.mjs";
+import { S3 } from '@aws-sdk/client-s3';
+import { log } from './log.mjs'
+
+const s3Client = new S3({ region: 'us-east-1' })
 export const handler = async (event) => {
     // TODO implement
-    log('handler no index.mjs event: ' + JSON.stringify(event))
+    const record = event.Records[0];
+    const Bucket = record.s3.bucket.name;
+    const Key = record.s3.object.key;
+    const getObjectResult = await s3Client.getObject({
+        Bucket,
+        Key,
+    })
 
-    return {
-        statusCode: 200,
-        body: `<html><body>Dadps da requisicao ${JSON.stringify(event)}</body></html>`,
-        headers: {
-            "content-type": "text/html"
-        }
-    };
+    const mega_byte = 1024 * 1024;
+
+    if (getObjectResult.ContentLength > 1 * mega_byte) {
+        log("Objeto muito grande")
+        return "Objeto muito grande"
+    }
+
+    log("Objeto de tamanho OK")
+    return "Objeto de tamanho OK"
 };
